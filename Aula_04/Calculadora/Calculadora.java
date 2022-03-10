0481a23160a488594b7b7265e8c3ec837157ec4f
Felipe Freitas Silva <75501741+felipefreitassilva@users.noreply.github.com>
2022-03-09 21:40:29 -0300

    JavaClass01
    Primeira aula de java e alguns rascunhos de código .java

---
+++ Aula_04/Calculadora/Calculadora.java
@@ -0,0 +1,10 @@
+public class Calculadora {
+    //4- Escreva um programa que calcula a soma dos dez primeiros números positivos, 1 + 2 + ... + 10
+    public static void main() {
+        for (int index = 1; index <= 10; index++) {
+            Number temp = index : int
+            index = index : int + temp : int
+            System.out.print(index)
+        }
+    }
+}
