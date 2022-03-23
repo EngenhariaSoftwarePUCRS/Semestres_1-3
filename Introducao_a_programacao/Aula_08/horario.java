74cc8ca2c3fbf13121fe054bc3d75266571751af
Felipe Freitas Silva <75501741+felipefreitassilva@users.noreply.github.com>
2022-03-23 20:14:30 -0300

    Ainda do Horário
    Finalmente resolvido exercício de conversão de horas militares

--- Introducao_a_programacao/Aula_08/horario.java
+++ Introducao_a_programacao/Aula_08/horario.java
@@ -4,19 +4,35 @@ public class horario {
         Scanner input = new Scanner(System.in);
         
         int horario1;
+        int hora_horario1;
+        int minuto_horario1;
+        int minutagem_horario1;
         int horario2;
+        int hora_horario2;
+        int minuto_horario2;
+        int minutagem_horario2;
         int diferenca;
+        int hora_diferenca;
+        int minuto_diferenca;
         
         System.out.println("Digite um primeiro horário: ");
         horario1 = input.nextInt();
-        
+        hora_horario1 = horario1 / 100;
+        minuto_horario1 = horario1 % 100;
+        minutagem_horario1 = 60 * hora_horario1 + minuto_horario1;
+        System.out.println(hora_horario1 + " hora(s) e " + minuto_horario1 + " minuto(s)."); 
+    
         System.out.println("Digite um segundo horário: ");
         horario2 = input.nextInt();
+        hora_horario2 = horario2 / 100;
+        minuto_horario2 = horario2 % 100;
+        minutagem_horario2 = 60 * hora_horario2 + minuto_horario2;
+        System.out.println(hora_horario2 + " hora(s) e " + minuto_horario2 + " minuto(s)."); 
         
-        diferenca = (horario1 - horario2);
-        Integer.toString(diferenca);
+        diferenca = (minutagem_horario1 - minutagem_horario2);
+        hora_diferenca = diferenca / 60;
+        minuto_diferenca = diferenca % 60;
         
-        System.out.println("Horário 1: "+horario1+" \nHorário 2: "+horario2);
-        System.out.print("Diferença: "+diferenca);
+        System.out.print("Diferença: "+hora_diferenca+" hora(s) e "+minuto_diferenca+" minuto(s).");
     }
 }
