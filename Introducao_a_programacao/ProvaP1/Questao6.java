b7c526588fecb8b834155931cd6b6c2c8652f25c
Felipe Freitas Silva <75501741+felipefreitassilva@users.noreply.github.com>
2022-05-02 20:32:04 -0300

    ProvaP1
    Exercícios da P1 revisados e digitalizados.

---
+++ Introducao_a_programacao/ProvaP1/Questao6.java
@@ -0,0 +1,8 @@
+public class Questao6 {
+    public static void main() {
+        String str = "Harry";
+        int n = str.length();
+        String mystery = str.replace("r", "n");
+        System.out.println(mystery + " - " + n);
+    }
+}
