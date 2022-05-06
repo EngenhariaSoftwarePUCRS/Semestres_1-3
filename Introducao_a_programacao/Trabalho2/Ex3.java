4eb0ed4b8a9ac0dd71f3bcbf6f94f4a0b45bc1cc
Felipe Freitas Silva <75501741+felipefreitassilva@users.noreply.github.com>
2022-05-06 20:36:54 -0300

    Trabalho2Loops
    Ex1-6,11,12

---
+++ Introducao_a_programacao/Trabalho2/Ex3.java
@@ -0,0 +1,24 @@
+/*3.	Escreva um programa que leia uma frase de um usuário e retorna quantos espaços em branco existem nessa frase.*/
+
+import java.util.Scanner;
+public class Ex3
+{
+    public static void main() {
+        String frase;
+        char caracter;
+        int qtdEspacos = 0;
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Digite uma palavra: ");
+        frase = input.nextLine();
+        
+        frase = frase.toUpperCase();
+        for (int i = 0; i < frase.length(); i++) {
+            caracter = frase.charAt(i);
+            if (caracter == 32) {
+                qtdEspacos++;
+            }
+        }
+        System.out.printf("Há %d espaços na frase %n", qtdEspacos);
+    }
+}
---
+++ Introducao_a_programacao/Trabalho2/Ex4.java
@@ -0,0 +1,35 @@
+/*4.Escreva um programa que lê um conjunto de números em ponto flutuante e imprime:
+A média dos valores
+O menor valor.
+O maior valor.
+A diferença entre o maior e o menor número digitado.*/
+
+import java.util.Scanner;
+public class Ex4
+{
+    public static void main() {
+        double numeroLido, menorValor, maiorValor;
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Digite valores diferentes de 0: ");
+        numeroLido = input.nextDouble();
+        menorValor = numeroLido;
+        maiorValor = numeroLido;
+        
+        while (numeroLido != 0) {
+            System.out.print("Digite um valor: ");
+            numeroLido = input.nextDouble();
+            
+            if (numeroLido > maiorValor) {
+                maiorValor = numeroLido;
+            }
+            if (numeroLido < menorValor) {
+                menorValor = numeroLido;
+            }
+        }
+        
+        System.out.println("Menor valor: "+menorValor);
+        System.out.println("Maior valor: "+maiorValor);
+        System.out.println("Diferença: "+(maiorValor - menorValor));
+    }
+}
