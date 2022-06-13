8c927c75d420ac8f625dea43d9431cf7ab670fe7
Felipe Freitas Silva <75501741+felipefreitassilva@users.noreply.github.com>
2022-06-13 15:29:58 -0300

    Players system implemented
    Amount of players and their names are variable
    Scores are also kept track of
    New method for determining podium and removing/returning values to a vector

--- Introducao_a_programacao/Trabalho3/Main.java
+++ Introducao_a_programacao/Trabalho3/Main.java
@@ -1,3 +1,4 @@
+
 /**
  O objetivo desse trabalho é criar um jogo que permita a disputa entre dois jogadores (jogador 1 e
 jogador 2).
@@ -18,95 +19,188 @@ tenham a mesma pontuação o jogo termina em empate.
  */
 import java.util.Random;
 import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
+        String[] players = getPlayers(input);
         int lineAmount = getLineAmount(input);
         int columnAmount = getColumnAmount(input);
         int lowerBound = getLowerBound(input);
         int upperBound = getUpperBound(input);
+        int i;
         int guess;
-        int score = 0;
-        int[][] matrix;
-        int[] valoresMatrix;
+        int qtdJogadas = 0;
+        int[][] matrix = populateMatrix(lineAmount, columnAmount, lowerBound, upperBound);
+        int[] valoresMatrix = getValues(matrix, lineAmount, columnAmount);
+        int[] scores = new int[players.length];
+        boolean acertou;
 
-        matrix = populateMatrix(lineAmount, columnAmount, lowerBound, upperBound);
-        valoresMatrix = getValues(matrix, lineAmount, columnAmount);
-
-        printMatrix(matrix, lineAmount, columnAmount);
-        do {
+        while (notEmpty(valoresMatrix)) {
+            acertou = false;
+            i = qtdJogadas % players.length;
+            System.out.printf("%n%s, ", players[i]);
             guess = getGuess(lowerBound, upperBound, input);
-            boolean Break = false;
-            for (int i = 0; i < lineAmount; i++) {
-                for (int j = 0; j < columnAmount; j++) {
-                    if (guess == matrix[i][j]) {
-                        System.out.println("Parabéns! Você ganhou um ponto. ");
-                        score++;
-                        matrix[i][j] = -1;
-                        Break = true;
-                        break;
-                    }
-                    if (Break)
-                        break;
+
+            if (guess == -1)
+                break;
+
+            for (int valor : valoresMatrix) {
+                if (guess == valor) {
+                    System.out.println("Parabéns! Você ganhou um ponto.");
+                    removeFrom(valoresMatrix, valor, true);
+                    scores[i] += 1;
+                    acertou = true;
+                    break;
                 }
             }
-        } while (score < 9);
+
+            if (!acertou) {
+                System.out.println("Infelizmente, não foi dessa vez.");
+            }
+
+            qtdJogadas++;
+            finalScore(qtdJogadas, players, scores);
+        }
+
+        finalScore(players, scores);
+        // printMatrix(matrix, lineAmount, columnAmount);
+    }
+
+    private static void printMatrix(int[][] matrix, int lineAmount, int columnAmount) {
+        for (int i = 0; i < lineAmount; i++) {
+            for (int j = 0; j < columnAmount; j++) {
+                System.out.printf("[%d]\t", matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    private static String[] orderPodium(String[] players, int[] scores) {
+        int maior;
+        int[] orderedScores = new int[scores.length];
+        String[] orderedPlayers = new String[players.length];
+
+        for (int i = 0; i < scores.length; i++) {
+            maior = -1;
+            for (int j = 0; j < scores.length; j++) {
+                if (scores[j] >= maior) {
+                    maior = scores[j];
+                }
+            }
+            orderedScores[i] = maior;
+            orderedPlayers[i] = players[indexOf(maior, scores)];
+            removeFrom(scores, maior, false);
+        }
+        returnTo(scores, orderedScores);
+
+        return orderedPlayers;
+    }
+
+    private static int indexOf(int a, int[] array) {
+        int i = 0;
+
+        if (array == null)
+            return -1;
+
+        while (i < array.length) {
+            if (array[i] == a)
+                return i;
+            else
+                i += 1;
+        }
+
+        return -1;
+    }
+
+    private static void finalScore(String[] players, int[] scores) {
+        players = orderPodium(players, scores);
+
+        System.out.println("/======================================\\");
+        System.out.println("\tPLACAR FINAL\t");
+        for (int i = 0; i < players.length; i++) {
+            System.out.printf("| %dº lugar: %s, com %d pontos. %n", i + 1, players[i], scores[i]);
+        }
+        System.out.println("\\======================================/");
+    }
+
+    private static void finalScore(int qtdJogadas, String[] players, int[] scores) {
+        System.out.println("Rodada: " + qtdJogadas);
+        for (int i = 0; i < players.length; i++) {
+            System.out.printf("%s tem %d pontos.%n", players[i], scores[i]);
+        }
+    }
+
+    private static void returnTo(int[] vector, int[] newVector) {
+        for (int i = 0; i < vector.length; i++)
+            vector[i] = newVector[i];
+
+    }
+
+    private static void removeFrom(int[] vector, int value, boolean all) {
+        for (int i = 0; i < vector.length; i++) {
+            if (value == vector[i]) {
+                vector[i] = -1;
+                if (!all)
+                    break;
+            }
+        }
     }
 
     private static int getGuess(int lowerBound, int upperBound, Scanner input) {
         int guess;
+
         do {
-            System.out.printf("Digite um número entre %d e %d: ", lowerBound, upperBound);
+            System.out.printf("digite um número entre %d e %d: ", lowerBound, upperBound);
             guess = input.nextInt();
-        } while (guess < lowerBound || guess > upperBound);
+        } while ((guess < lowerBound || guess > upperBound) && (guess != -1));
+
         return guess;
     }
 
+    private static boolean notEmpty(int[] vetor) {
+        for (int i : vetor) {
+            if (i != -1)
+                return true;
+        }
+        return false;
+    }
+
     private static int[] getValues(int[][] matrix, int lineAmount, int columnAmount) {
-        int[] valoresMatrix = new int[lineAmount*columnAmount];
         int k = 0;
+        int[] valoresMatrix = new int[lineAmount * columnAmount];
+
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix.length; j++) {
                 valoresMatrix[k] = matrix[i][j];
                 k++;
             }
         }
+
         return valoresMatrix;
+    }
+
+    private static int getRandom(int lowerBound, int upperBound) {
+        Random r = new Random();
+        return r.nextInt(upperBound - lowerBound + 1) + lowerBound;
     }
 
     private static int[][] populateMatrix(int lineAmount, int columnAmount, int lowerBound, int upperBound) {
         int[][] matrix = new int[lineAmount][columnAmount];
+
         for (int i = 0; i < lineAmount; i++) {
             for (int j = 0; j < columnAmount; j++) {
                 matrix[i][j] = getRandom(lowerBound, upperBound);
             }
         }
+
         return matrix;
     }
 
-    // private static void printMatrix(int[][] matrix, int lineAmount, int columnAmount) {
-    //     for (int i = 0; i < lineAmount; i++) {
-    //         for (int j = 0; j < columnAmount; j++) {
-    //             System.out.printf("[%d]\t", matrix[i][j]);
-    //         }
-    //         System.out.println();
-    //     }
-    // }
-
-    private static int getRandom(int lowerBound, int upperBound) {
-        Random r = new Random();
-        int random = r.nextInt(upperBound - lowerBound+1) + lowerBound;
-        return random;
-    }
-
-    private static int getLineAmount(Scanner input) {
-        System.out.print("Digite a quantidade de linhas da matriz: ");
-        return input.nextInt();
-    }
-
-    private static int getColumnAmount(Scanner input) {
-        System.out.print("Digite a quantidade de colunas da matriz: ");
+    private static int getUpperBound(Scanner input) {
+        System.out.print("Digite o maior valor que você quer na matriz: ");
         return input.nextInt();
     }
 
@@ -115,8 +209,31 @@ public class Main {
         return input.nextInt();
     }
 
-    private static int getUpperBound(Scanner input) {
-        System.out.print("Digite o maior valor que você quer na matriz: ");
+    private static int getColumnAmount(Scanner input) {
+        System.out.print("Digite a quantidade de colunas da matriz: ");
         return input.nextInt();
+    }
+
+    private static int getLineAmount(Scanner input) {
+        System.out.print("Digite a quantidade de linhas da matriz: ");
+        return input.nextInt();
+    }
+
+    private static String[] getPlayers(Scanner input) {
+        int qtdJogadores;
+        String[] players;
+
+        System.out.print("Digite a quantidade de jogadores: ");
+        qtdJogadores = input.nextInt();
+        players = new String[qtdJogadores];
+        int i = 0;
+        input.nextLine();
+        while (i < qtdJogadores) {
+            System.out.printf("Digite o nome do %dº jogador: ", i + 1);
+            players[i] = input.nextLine();
+            i++;
+        }
+
+        return players;
     }
 }
