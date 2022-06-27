df909bd325626c13e980ea7d20570fa84e199b7f
Felipe Freitas Silva <75501741+felipefreitassilva@users.noreply.github.com>
2022-06-27 15:33:42 -0300

    Update Perimetro_do_quadrado.js

--- Calculo_I/Aula_01/Perimetro_do_quadrado.js
+++ Calculo_I/Aula_01/Perimetro_do_quadrado.js
@@ -1,9 +1,10 @@
 var quantidade = 0;
-var a = 0.5
-var b = 0.5
-for (let x = 0; x<=450; x+=a) {
+var a = 1
+var b = 1
+var area = 5000
+for (let x = 0; x <= area; x+=a) {
     for (let y = 0; y <= 450; y+=b) {
-        if (x*y == 450) {
+        if (x*y == area) {
             var perimetro = 2*x + 2*y
             console.log("Base: "+x+" Altura: "+y)
             console.log("Perímetro: "+perimetro)
