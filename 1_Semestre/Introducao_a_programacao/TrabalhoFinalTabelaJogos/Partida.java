cdca3a1e160eb5f1b9afba73dda3f0f98ba269af
Felipe Freitas Silva <75501741+felipefreitassilva@users.noreply.github.com>
2022-07-01 01:16:42 -0300

    Update Partida.java

--- 1_Semestre/Introducao_a_programacao/TrabalhoFinalTabelaJogos/Partida.java
+++ 1_Semestre/Introducao_a_programacao/TrabalhoFinalTabelaJogos/Partida.java
@@ -1,9 +1,9 @@
 public class Partida {
 
-    private int numeroPartida = 0;
-    private Data data = new Data();
-    private Time[] times = new Time[2];
-    private int[] placarPartida = new int[2];
+    private int numeroPartida;
+    private Data data;
+    private Time[] times;
+    private int[] placarPartida;
 
     public Partida(Data data, Time[] times, int[] placarPartida) {
         this.data = data;
@@ -34,8 +34,8 @@ public class Partida {
     public String toString() {
         return ("\n" + numeroPartida + "ª partida" +
                 "\n" + data +
-                "\nPrimeiro time = " + times[0].getNomeTime() +
-                "\tSegundo time = " + times[1].getNomeTime() +
+                "\nPrimeiro time = " + getTime1().getNomeTime() +
+                "\tSegundo time = " + getTime2().getNomeTime() +
                 "\nPlacar = (" + placarPartida[0] + " x " + placarPartida[1] + ")");
     }
 
