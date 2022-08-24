cd1ada86931deb28f8ba6374f847010d4b4b93ec
Felipe Freitas Silva <75501741+felipefreitassilva@users.noreply.github.com>
2022-08-24 20:31:44 -0300

    Update Main.java
    Lista 1, exercicios a, b, d
    Diferença entre pesquisa direta e binária

--- 2_Semestre/Alest/src/Aula05/Main.java
+++ 2_Semestre/Alest/src/Aula05/Main.java
@@ -3,9 +3,39 @@ package Aula05;
 import java.util.Random;
 
 public class Main {
+
+    private static int contador = 0;
+
     public static void main(String[] args) {
-        int[] meuVetor = geraVetor(10000, 5000);
-        binarySearch(meuVetor, 40);
+        System.out.println("Com pesquisa direta: ");
+        int[] meuVetorDezMil = bubbleSortV1(geraVetor(10000, 10000));
+        // int[] meuVetorUmMilhao = bubbleSortV1(geraVetor(1000000, 1000000));
+        System.out.println("Posição do número 100 no vetor: " + pesqDireta(meuVetorDezMil, 20));
+        System.out.println("Quantidade de iterações em vetor de 10.000 posições: " + contador);
+        contador = 0;
+        // System.out.println("Posição do número 100 no vetor: " + pesqDireta(meuVetorUmMilhao, 100));
+        // System.out.println("Quantidade de iterações em vetor de 1.000.000 posições: " + contador);
+        // contador = 0;
+
+        System.out.println("===============================================================");
+
+        System.out.println("Com pesquisa binária: ");
+        System.out.println("Posição do número 100 no vetor: " + binarySearch(meuVetorDezMil, 20));
+        System.out.println("Quantidade de iterações em vetor de 10.000 posições: " + contador);
+        contador = 0;
+        // System.out.println("Posição do número 100 no vetor: " + binarySearch(meuVetorUmMilhao, 100));
+        // System.out.println("Quantidade de iterações em vetor de 1.000.000 posições: " + contador);
+        // contador = 0;
+    }
+
+    static int pesqDireta(int[] vet, int valor) {
+        int res = -1, i;
+        for (i = 0; ((i < vet.length) && (vet[i] != valor)); i++) {
+            if (i < vet.length)
+                res = i;
+            contador++;
+        }
+        return res;
     }
 
     static int binarySearch(int[] v, int value) {
@@ -13,6 +43,7 @@ public class Main {
         int high = v.length - 1;
         int p = low + ((high - low) / 2);
         while (low <= high) {
+            contador++;
             if (v[p] > value)
                 high = p - 1;
             else if (v[p] < value)
@@ -24,13 +55,14 @@ public class Main {
         return -1;
     }
 
-    public static void bubbleSortV1(int[] v) {
+    public static int[] bubbleSortV1(int[] v) {
         for (int i = 0; i < v.length; i++) {
             for (int j = 0; j < v.length - 1; j++) {
                 if (v[j] > v[j + 1])
-                    troca(v, j, j + 1);
+                    v = troca(v, j, j + 1);
             }
         }
+        return v;
     }
 
     public static void bubbleSortV2(int[] v) {
@@ -46,10 +78,11 @@ public class Main {
         }
     }
 
-    private static void troca(int[] v, int i, int j) {
+    private static int[] troca(int[] v, int i, int j) {
         int aux = v[i];
         v[i] = v[j];
         v[j] = aux;
+        return v;
     }
 
     public static int[] geraVetor(int nroElem, int lim) {
