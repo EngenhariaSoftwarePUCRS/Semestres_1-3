d165fd81571a3eef75f081b6ad64f01b96fd16ad
Felipe Freitas Silva <75501741+felipefreitassilva@users.noreply.github.com>
2022-09-26 21:48:30 -0300

    Por continuar

---
+++ 2_Semestre/POO/src/Aula15/Modalidade.java
@@ -0,0 +1,5 @@
+package Aula15;
+
+public enum Modalidade {
+    CORRIDA, NATACAO, FUTEBOL, VOLEI, BASQUETE, TENIS, NONE
+}
---
+++ 2_Semestre/POO/src/Aula15/PessoaCadastrada.java
@@ -0,0 +1,5 @@
+package Aula15;
+
+public class PessoaCadastrada {
+    
+}
