f953ed1e17a4578d22007ad46eb08281bf63fb86
Felipe Freitas Silva <75501741+felipefreitassilva@users.noreply.github.com>
2022-10-14 09:39:29 -0300

    Update Sistema.java
    Included all functions in the main Sistema file

--- 2_Semestre/Alest/src/Trabalho01/Sistema.java
+++ 2_Semestre/Alest/src/Trabalho01/Sistema.java
@@ -1,61 +1,129 @@
 package Trabalho01;
 
-import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Sistema {
-	
-	public static void main(String[] args) {
-		ArrayList<NumeroComplexo> lst = new ArrayList<>();
-		String[] operacoes = {"soma", "subtração", "inverso", "changeSign", "conjugado", "modulo"};
+    Stack<NumeroComplexo> pilha = new Stack<>();
 
-		NumeroComplexo a = new NumeroComplexo(1, -2);
-		NumeroComplexo b = new NumeroComplexo(-2, -3);
-		NumeroComplexo c = new NumeroComplexo(5, 2);
-		lst.add(sum(a, b));
-		lst.add(sub(a, b));
-		// lst.add(mul(a, b));
-		// lst.add(div(a, b));
-		lst.add(inv(c));
-		lst.add(chs(c));
-		lst.add(conj(c));
-		lst.add(abs(c));
+    public void inicializar(String fileName) throws FileNotFoundException {
+        try {
+            File calculos = new File(fileName);
+            Scanner reader = new Scanner(calculos);
+            String next;
+            while (reader.hasNext()) {
+                next = reader.nextLine();
+                newOperation(next);
+            }
+            reader.close();
+        } catch (FileNotFoundException fnfe) {
+            throw new FileNotFoundException();
+        }
+    }
 
-		System.out.println("{");
-		for (int i = 0; i < lst.size(); i++) {
-			System.out.println("\t" + operacoes[i] + ": " + lst.get(i)+", ");
-		}
-		System.out.println("}");
-	}
+    static final String ERROR_MSG = "Não há valores disponíveis para realizar a operação.";
 
-	static NumeroComplexo sum(NumeroComplexo a, NumeroComplexo b) {
-		return CalculadoraComplexo.soma(a, b);
-	}
+    void newOperation(String next) {
+        NumeroComplexo a, b;
+        if (!stackNumbers(next))
+            switch (next) {
+                case "+":
+                    a = pop();
+                    b = pop();
+                    add(CalculadoraComplexo.soma(a, b));
+                    break;
 
-	static NumeroComplexo sub(NumeroComplexo a, NumeroComplexo b) {
-		return CalculadoraComplexo.subtracao(a, b);
-	}
+                case "-":
+                    a = pop();
+                    b = pop();
+                    add(CalculadoraComplexo.subtracao(a, b));
+                    break;
 
-	static NumeroComplexo mul(NumeroComplexo a, NumeroComplexo b) {
-		return CalculadoraComplexo.multiplicacao(a, b);
-	}
+                case "*":
+                    a = pop();
+                    b = pop();
+                    add(CalculadoraComplexo.multiplicacao(a, b));
+                    break;
 
-	static NumeroComplexo div(NumeroComplexo a, NumeroComplexo b) {
-		return CalculadoraComplexo.divisao(a, b);
-	}
+                case "/":
+                    a = pop();
+                    b = pop();
+                    add(CalculadoraComplexo.divisao(a, b));
+                    break;
 
-	static NumeroComplexo inv(NumeroComplexo n) {
-		return CalculadoraComplexo.inverso(n);
-	}
+                case "inv":
+                    a = pop();
+                    add(CalculadoraComplexo.inverso(a));
+                    break;
 
-	static NumeroComplexo chs(NumeroComplexo n) {
-		return CalculadoraComplexo.changeSign(n);
-	}
+                case "chs":
+                    a = pop();
+                    add(CalculadoraComplexo.changeSign(a));
+                    break;
 
-	static NumeroComplexo conj(NumeroComplexo n) {
-		return CalculadoraComplexo.conjugado(n);
-	}
+                case "conj":
+                    a = pop();
+                    add(CalculadoraComplexo.conjugado(a));
+                    break;
 
-	static NumeroComplexo abs(NumeroComplexo n) {
-		return new NumeroComplexo(CalculadoraComplexo.absolute(n), 0);
-	}
+                case "abs":
+                    a = pop();
+                    add(new NumeroComplexo(CalculadoraComplexo.absolute(a), 0));
+                    break;
+
+                case "pop":
+                    pop();
+                    break;
+
+                case "dup":
+                    duplica();
+                    break;
+
+                case "swap":
+                    swap();
+                    break;
+
+                default:
+                    System.out.println("Operação (" + next + ") inválida.");
+                    break;
+            }
+    }
+
+    void add(NumeroComplexo n) {
+        pilha.push(n);
+    }
+
+    boolean stackNumbers(String next) {
+        try {
+            String[] numeros = next.split(" ");
+            double parteReal = Double.parseDouble(numeros[0]);
+            double parteImaginaria = Double.parseDouble(numeros[1]);
+            add(new NumeroComplexo(parteReal, parteImaginaria));
+            return true;
+        } catch (NumberFormatException nfe) {
+            System.out.println(nfe);
+            return false;
+        }
+    }
+
+    NumeroComplexo pop() {
+        return pilha.pop();
+    }
+
+    void duplica() {
+        NumeroComplexo novo = new NumeroComplexo(pop());
+        pilha.add(novo);
+        pilha.add(novo);
+    }
+
+    void swap() {
+        if (pilha.size() > 1) {
+            NumeroComplexo aux1 = pop();
+            NumeroComplexo aux2 = pop();
+            pilha.add(aux2);
+            pilha.add(aux1);
+        } else
+            System.out.println(ERROR_MSG);
+    }
 }
