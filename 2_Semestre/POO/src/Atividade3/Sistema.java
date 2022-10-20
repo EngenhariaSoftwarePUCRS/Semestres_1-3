015d50e34755b465497435c18e91d1e3a737afb3
Felipe Freitas Silva <75501741+felipefreitassilva@users.noreply.github.com>
2022-10-19 23:06:14 -0300

    Update Sistema.java
    Now file is rewritten backwards

--- 2_Semestre/POO/src/Atividade3/Sistema.java
+++ 2_Semestre/POO/src/Atividade3/Sistema.java
@@ -9,20 +9,8 @@ import java.util.Scanner;
 
 public class Sistema {
     public void inicializar() {
-        Scanner input = new Scanner(System.in);
-        String fileName;
-        File file;
-        
-        do {
-            System.out.print("Nome do arquivo que deseja ler: ");
-            fileName = input.nextLine();
-            file = new File(fileName);
-            if (!file.exists())
-                System.out.println("Arquivo inexistente.\nTente novamente.");
-        } while(!file.exists());
-
         try {
-            renumber(file);
+            renumber(getFile());
         } catch (FileNotFoundException fnfe) {
             System.out.println(fnfe);
         } catch (IOException ioe) {
@@ -30,28 +18,42 @@ public class Sistema {
         }
     }
 
-    private void renumber(File file) throws IOException {
-        Scanner reader;
+    private File getFile() {
+        Scanner input = new Scanner(System.in);
+        String fileName;
+        File file;
+
+        System.out.print("Nome do arquivo que deseja ler: ");
+        fileName = input.nextLine();
+        file = new File(fileName);
+        if (!file.exists()) {
+            System.out.println("Arquivo inexistente.\nTente novamente.");
+            return getFile();
+        }
+
+        return file;
+    }
+
+    void renumber(File file) throws IOException {
         ArrayList<String> lineNumbers = getLineNumbers(file);
-        String[] lines = new String[lineNumbers.size()];
+        String[] lines = getLines(file);
         File novo = new File(file.getName().split("\\.")[0].concat("-rn.").concat(file.getName().split("\\.")[1]));
         FileWriter fw = new FileWriter(novo);
-        String oldLine, newLine;
+        String oldLine;
+        String newLine;
 
-        for (int i = 1; i <= lines.length; i++) {
-            reader = new Scanner(file);
-            String velha = lineNumbers.get(0);
+        for (int i = lines.length; i > 0; i--) {
+            String velha = lineNumbers.get(lineNumbers.size() - 1);
             String nova = Integer.toString(i * 10);
-            lineNumbers.remove(0);
-            for (int j = 0; reader.hasNext(); j++) {
-                oldLine = reader.nextLine();
+            lineNumbers.remove(lineNumbers.size() - 1);
+            for (int j = 0; j < lines.length; j++) {
+                oldLine = lines[j];
                 newLine = oldLine.replaceAll(velha, nova);
-                if (i - 1 == j)
-                    lines[j] = newLine;
+                lines[j] = newLine;
             }
-            fw.write(lines[i - 1] + "\n");
-            // System.out.println("Linha "+(i-1)+": "+lines[i - 1]);
         }
+        for (String line : lines)
+            fw.write(line + "\n");
         fw.close();
     }
 
@@ -63,5 +65,27 @@ public class Sistema {
             lineNumbers.add(reader.nextLine().split(" ")[0]);
 
         return lineNumbers;
+    }
+
+    private int getLineAmount(File file) throws IOException {
+        Scanner reader = new Scanner(file);
+        int lineAmount = 0;
+
+        while (reader.hasNext()) {
+            lineAmount++;
+            reader.nextLine();
+        }
+
+        return lineAmount;
+    }
+
+    private String[] getLines(File file) throws IOException {
+        Scanner reader = new Scanner(file);
+        String[] lines = new String[getLineAmount(file)];
+
+        for (int i = 0; i < lines.length; i++)
+            lines[i] = reader.nextLine();
+
+        return lines;
     }
 }
