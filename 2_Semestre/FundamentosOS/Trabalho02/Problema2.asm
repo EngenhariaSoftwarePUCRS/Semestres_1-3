78531c5d339c3cd90896a1d124a3997ecb5c6d23
Felipe Freitas Silva <75501741+felipefreitassilva@users.noreply.github.com>
2022-10-28 08:51:10 -0300

    Questão 2 finalizada

--- 2_Semestre/FundamentosOS/Trabalho02/Problema2.asm
+++ 2_Semestre/FundamentosOS/Trabalho02/Problema2.asm
@@ -26,69 +26,164 @@
 ;     }
 ; }
 
-v1 {
+vFinal {
 main
-    ; r1 = soma(começa em 0)
+	; r1 = soma(começa em 0)
 
-    ; r2 = sequencia
-    ldi r2,sequencia
+	; r2 = sequencia
+	ldi r2,sequencia
 
-    ; r3 = segmento
-    ldi r3,segmento
+	; r3 = segmento
+	ldi r3,segmento
 
-    ; r4 = segmento[0]
-    ldw r4,r3
+	; salva r1,r2
+	sub sp,2
+	stw r1,sp
+	sub sp,2
+	stw r2,sp
 
-    ; r5 = segmento[1]
-    add r3,2
-    ldw r5,r3
+	; r4 = segmento[0]
+	ldw r4,r3
 
-    ; r6 = 8
-    ldc r6,8
+	; r5 = segmento[1]
+	add r3,2
+	ldw r5,r3
 
-verificacao_soma
-    ; r3 = 0
-    ldi r3,0
+	; r1 = r2 = 0
+	ldi r1,0
+	ldi r2,0
 
-    ; r3 = r4 < r5 ? 1 : 0
-    slt r3,r4,r5
-    bnz r3,laco_soma
-    bnz r7,print
+	; r1 = r5 - r4
+	sub r1,r5,r4
+
+	; r6 = r1 (quantas vezes repito o laco)
+	xor r6,r1,r2
+
+	; recupera r1,r2
+	ldw r2,sp
+	add sp,2
+	ldw r1,sp
+	add sp,2
+
+	; r2 -> sequence[-1]
+	sub r2,2
+
+	; r5 = -1
+	ldi r5,-1
+
+move_pointer
+	; r2 -> sequence[+1]
+	add r2,2
+
+	; r3 = 0
+	ldi r3,0
+
+	; r4--;
+	sub r4,1
+
+	; if (r4 > 0) {
+	slt r3,r5,r4
+
+	; repeat }
+	bnz r3,move_pointer
+
+volta
+	; r2 -> sequence[-1]
+	sub r2,2
 
 laco_soma
-    ; r3 = 0
-    ldi r3,0
+	; r2 -> sequence[+1]
+	add r2,2
 
-    ; r3 = sequencia[0]
-    ldw r3,r2
+	; r3 = r4 = 0
+	ldi r3,0
 
-    ; r1 += r3
-    add r1,r3,r1
+	; r3 = sequencia[i]
+	ldw r3,r2
 
-    ldi r3,0
-    sub r6,1
-    slt r3,1,r6
-    bez r3,print
-    bnz r7,repetir
+	; r1 += r3
+	add r1,r3,r1
 
-repetir
-    ; sequencia[0+1]
-    add r2,2
+	; r6--
+	sub r6,1
 
-    ; r3 = 0
-    ldi r3,0
+	; if (r6 > 0) {
+	slt r4,r5,r6
 
-    ; r3 = sequencia[0]
-    ldw r3,r2
-
-    ; r1 += r3
-    add r1,r3,r1
-    bnz r7,laco_soma
+	; repeat }
+	bnz r4,laco_soma
 
 print
-    stw r1,0xf002
-    hcf
+	stw r1,0xf002
+	hcf
 
 sequencia 5 2 -1 -7 3 14 10 -3 9 -6 4 1
-segmento 0 4
+segmento 4 8
+}
+
+v1 {
+    main
+        ; r1 = soma(começa em 0)
+
+        ; r2 = sequencia
+        ldi r2,sequencia
+
+        ; r3 = segmento
+        ldi r3,segmento
+
+        ; r4 = segmento[0]
+        ldw r4,r3
+
+        ; r5 = segmento[1]
+        add r3,2
+        ldw r5,r3
+
+        ; r6 = 8
+        ldc r6,8
+
+    verificacao_soma
+        ; r3 = 0
+        ldi r3,0
+
+        ; r3 = r4 < r5 ? 1 : 0
+        slt r3,r4,r5
+        bnz r3,laco_soma
+        bnz r7,print
+
+    laco_soma
+        ; r3 = 0
+        ldi r3,0
+
+        ; r3 = sequencia[0]
+        ldw r3,r2
+
+        ; r1 += r3
+        add r1,r3,r1
+
+        ldi r3,0
+        sub r6,1
+        slt r3,1,r6
+        bez r3,print
+        bnz r7,repetir
+
+    repetir
+        ; sequencia[0+1]
+        add r2,2
+
+        ; r3 = 0
+        ldi r3,0
+
+        ; r3 = sequencia[0]
+        ldw r3,r2
+
+        ; r1 += r3
+        add r1,r3,r1
+        bnz r7,laco_soma
+
+    print
+        stw r1,0xf002
+        hcf
+
+    sequencia 5 2 -1 -7 3 14 10 -3 9 -6 4 1
+    segmento 0 4
 }
