de2715509a9566702979d09ca53475279045c17f
Felipe Freitas Silva <eu.felipefreitassilva@gmail.com>
2023-03-07 14:51:59 -0300

    3S_AGESI_PDFRead
    More testing, now with area striping

--- 3_Semestre/AGES I/PDFRead/README.md
+++ 3_Semestre/AGES I/PDFRead/README.md
@@ -2,4 +2,4 @@
 
 -   https://pdfbox.apache.org/
 -   Download: (https://dlcdn.apache.org/pdfbox/2.0.27/pdfbox-app-2.0.27.jar)
-
+-   Examples: (https://svn.apache.org/viewvc/pdfbox/trunk/examples/src/main/java/org/apache/pdfbox/examples/)
--- 3_Semestre/AGES I/PDFRead/src/App.java
+++ 3_Semestre/AGES I/PDFRead/src/App.java
@@ -4,16 +4,31 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
 import org.apache.pdfbox.text.PDFTextStripperByArea;
 
+import java.awt.Rectangle;
+import org.apache.pdfbox.pdmodel.PDPage;
+
 public class App {
     public static void main(String[] args) {
         try {
-            PDDocument document = PDDocument.load(new File("src/vmtap.pdf"));
+            PDDocument document = PDDocument
+                    .load(new File("../Veiculos Montadora - Termo de Abertura do Projeto.pdf"));
 
-            if (!document.isEncrypted()) {
-                PDFTextStripper stripper = new PDFTextStripper();
-                String text = stripper.getText(document);
-                System.out.println("Text:" + text);
-            }
+            if (document.isEncrypted())
+                throw new Exception("File is encrypted");
+
+            // PDFTextStripper stripper = new PDFTextStripper();
+            // PDFTextStripperByArea areaStripper = new PDFTextStripperByArea();
+            // areaStripper.setSortByPosition(true);
+            // String text = stripper.getText(document);
+
+            PDFTextStripperByArea stripper = new PDFTextStripperByArea();
+            stripper.setSortByPosition(true);
+            Rectangle rect = new Rectangle(10, 280, 275, 60);
+            stripper.addRegion("class1", rect);
+            PDPage firstPage = document.getPage(0);
+            stripper.extractRegions(firstPage);
+            System.out.println("Text in the area:" + rect);
+            System.out.println(stripper.getTextForRegion("class1"));
 
             document.close();
         } catch (IOException e) {
