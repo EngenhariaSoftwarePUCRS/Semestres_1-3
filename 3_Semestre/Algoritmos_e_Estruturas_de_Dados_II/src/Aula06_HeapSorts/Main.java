18c72c970ee92a5a27946da3b7f5da3f4d93d8a4
Felipe Freitas Silva <eu.felipefreitassilva@gmail.com>
2023-03-17 19:22:35 -0300

    3S_ALEST_II_Aula06_HeapSorts
    TODO: Finish

---
+++ 3_Semestre/Algoritmos_e_Estruturas_de_Dados_II/src/Aula06_HeapSorts/Main.java
@@ -0,0 +1,22 @@
+package Aula06_HeapSorts;
+
+import aula05_filasdeprioridade_heap.HeapMaximo;
+
+public class Main {
+    public static void main(String[] args) {
+        HeapMaximo heap = new HeapMaximo(10);
+        heap.inserir(10);
+        heap.inserir(20);
+        heap.inserir(15);
+        heap.inserir(5);
+        heap.inserir(30);
+        heap.inserir(120);
+        heap.inserir(1);
+        heap.inserir(2);
+        heap.inserir(3);
+
+        for (int i : heap.ordernarHeapSort()) {
+            System.out.println(i);
+        }
+    }
+}
