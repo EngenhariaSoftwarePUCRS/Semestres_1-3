765e331071fe3365f2fc14704bfdb1a51c82c633
Felipe Freitas Silva <eu.felipefreitassilva@gmail.com>
2023-04-18 18:24:50 -0300

    3S_ALESTII_SimuladoP1

---
+++ 3_Semestre/Algoritmos_e_Estruturas_de_Dados_II/src/Aula15_SimuladoP1/Questao08.java
@@ -0,0 +1,46 @@
+public class Questao08 {
+    int[] hashTable;
+
+    public Questao08() {
+        hashTable = new int[7];
+        for (int i = 0; i < hashTable.length; i++)
+            hashTable[i] = -1;
+    }
+
+    public void inserir(int chave) throws Exception {
+        if (!isValid(chave))
+            return;
+        int hashIndex = chave % 7;
+        int valueInIndex = hashTable[hashIndex];
+        boolean free = valueInIndex == -1;
+        if (free) {
+            hashTable[hashIndex] = chave;
+            System.out.printf("Chave %s inserida com sucesso na posição %d%n", chave, hashIndex);
+            return;
+        }
+        if (valueInIndex == chave) {
+            System.out.println("Chave já existe");
+            return;
+        }
+        for (int i = 0; i < hashTable.length; i++)
+            if (hashTable[i] == -1) {
+                hashTable[i] = chave;
+                return;
+            }
+        throw new Exception("Não há mais espaço disponível");
+    }
+
+    public boolean isValid(int value) {
+        return (value >= 0 && value <= 1000);
+    }
+
+    public static void main(String[] args) {
+        Questao08 q = new Questao08();
+        try {
+            for (int i = 0; i < 1000; i++)
+                q.inserir((int) Math.floor(Math.random() * 1000));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ 3_Semestre/Algoritmos_e_Estruturas_de_Dados_II/src/Aula15_SimuladoP1/Questao09.java
@@ -0,0 +1,43 @@
+public class Questao09 {
+    // a) 00000000011111100000011111
+    // 9x0’s + 6x1's + 6x0’s + 5x1's = 26 bits
+    // 1001 0110 0110 0101 --> 16/26 = 61%
+
+    // b) 00000011111100000000111100
+    // 6x0's + 6x1's + 8x0's + 4x1's + 2x0's = 26 bits
+    // 0110 0110 1000 0100 0010 --> 20/26 = 76%
+
+    // c) 11111111111111111110000000
+    // 15x1's + 0x0's + 4x1's + 7x0's = 26 bits
+    // 1111 0000 0100 0111 --> 16/26 = 61%
+
+    public String decimalToBinary(int decimal) {
+        String binary = "";
+        while (decimal > 0) {
+            binary += decimal % 2;
+            decimal /= 2;
+        }
+        return binary;
+    }
+
+    public static void main(String[] args) {
+        String input = "00000000011111100000011111";
+        String output = "";
+        boolean isZeroCurrently = input.charAt(0) == '0';
+        int count = 0;
+        int maxCount = 15;
+        for (int i = 0; i < input.length(); i++) {
+            if (count > maxCount) {
+                maxCount = count;
+                isZeroCurrently = !isZeroCurrently;
+            }
+            if (input.charAt(i) == '0' && isZeroCurrently) {
+                count++;
+            } else if (input.charAt(i) == '1' && !isZeroCurrently) {
+                count++;
+            } else {
+                count = 0;
+            }
+        }
+    }
+}
