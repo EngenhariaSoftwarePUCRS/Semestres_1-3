c28a13f91f5396775398dc43aef4626120508625
Felipe Freitas Silva <eu.felipefreitassilva@gmail.com>
2023-04-18 20:46:23 -0300

    3S_BDII_MongoDB

---
+++ 3_Semestre/Banco_de_Dados_II/Semana07/index.js
@@ -0,0 +1,41 @@
+const { MongoClient } = require('mongodb');
+require('dotenv').config()
+
+async function main() {
+    /**
+     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+     */
+
+    const uri = process.env.CONNECTION_URI;
+
+    const client = new MongoClient(uri);
+
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+
+        // Make the appropriate DB calls
+        await listDatabases(client);
+
+        await client.db("sample_airbnb").collection("listingsAndReviews")
+            .find({})
+            .limit(5)
+            .forEach(console.log)
+
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+main().catch(console.error);
+
+async function listDatabases(client) {
+    databasesList = await client.db().admin().listDatabases();
+    console.log("Databases:");
+    databasesList.databases.forEach(db => {
+        console.log(` - ${db.name}`)
+    });
+};
