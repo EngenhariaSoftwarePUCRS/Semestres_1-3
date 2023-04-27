1c0576636d56be576c44a159cae1f1b2f6f2e3e1
Felipe Freitas Silva <eu.felipefreitassilva@gmail.com>
2023-04-27 01:47:29 -0300

    3S_DBII_T01_TarefaIII

--- 3_Semestre/Banco_de_Dados_II/Trabalho01/INSERT.sql
+++ 3_Semestre/Banco_de_Dados_II/Trabalho01/INSERT.sql
@@ -5,50 +5,50 @@ INSERT INTO Carro (Codigo, Placa, Marca, Modelo, Ano, Nome_motorista) VALUES
     (4, 'NEA6661', 'Fiat', 'Palio', 2010, 'Sônia Ana Corte Real');
     
 INSERT INTO Passageiro (CPF, Nome, Email, Telefone, Bairro, Cidade, Estado, Meio_Pagamento) VALUES
-	('40153021845', 'Julio Caleb Marcelo Corte Real Assis', 'julio_caleb_corterealassis@expressoforte.com.br', '61981253455', 'Ceilândia Norte', 'Ceilândia', 'Brasilia', 'Crédito MasterCard'),
-    ('43870718692', 'Marcelo Luiz Vieira', 'marcelo_vieira@studiodeideias.com', '79987733936', 'Cidade Nova', 'Belo Horizonte', 'Minas Gerais', 'Visa 16 Dígitos'),
-    ('98944351490', 'Marlene Mirella Assis', 'marlene_assis@contjulioroberto.com.br', '15992325358', 'Jardim Santa Inêz', 'Itapetininga', 'São Paulo', 'Visa Electron'),
-    ('64812811058', 'José Theo Giovanni Aragão', 'jose.theo.aragao@netsite.com.br', '81998804732', 'COHAB', 'Cachoeirinha', 'Rio Grande do Sul', 'Dinheiro'),
-    ('38598277312', 'Marcos Kaique Lorenzo Viana', 'marcos-viana88@terra.com.br', '6937921678', NULL, NULL, NULL, 'Débito Maestro'),
-    ('90897008200', 'Sabrina Ayla Nogueira', 'sabrinaaylanogueira@sygma.com.br', '85987842372', 'Presidente Kennedy', 'Fortaleza', 'Ceará', NULL);
+	('10531043088', 'Alexandre Rafael Julio Silveira Gonçalves', 'alexandrerafaelsilveira@gmail.com', '54981380067', 'Cerâmica', 'Erechim', 'Rio Grande do Sul', 'Crédito MasterCard'),
+    ('29438733060', 'Theo Danilo Brito', 'theo_danilo_brito@jh2consulting.com', '55988063073', 'Nossa Senhora de Fátima', 'Santa Maria', 'Rio Grande do Sul', 'Visa 16 Dígitos'),
+    ('98822414098', 'Raul Tiago Manuel da Silva', 'raul_dasilva@terra.com.br', '51991926194', 'São Vicente', 'Gravataí', 'Rio Grande do Sul', 'Visa Electron'),
+    ('59362314029', 'Agatha Maitê Aline Gonçalves', 'agatha.maite.goncalves@uol.com.br', '51997736328', 'Passo da Areia', 'Porto Alegre', 'Rio Grande do Sul', 'Dinheiro'),
+    ('78428732086', 'Rita Clarice Stella Melo', 'rita_clarice_melo@coldblock.com.br', '51991576603', NULL, NULL, NULL, 'Débito Maestro'),
+    ('42738597009', 'Liz Débora Fernandes', 'lizdeborafernandes@vhbadvogados.com.br', '55999817972', 'Floresta', 'Porto Alegre', 'Rio Grande do Sul', NULL);
     
 INSERT INTO Viagem (Id, Kms, Valor, Duracao, FK_Carro_Codigo) VALUES
-    (1, 10.5, 25.75, 30, 1),
-    (2, 15.2, 30.00, 40, 2),
-    (3, 5.7, 12.50, 20, 3),
-    (4, 20.0, 50.00, 50, 4),
-    (5, 8.5, 20.25, 25, 1),
-    (6, 12.0, 27.50, 35, 2),
-    (7, 3.5, 8.75, 15, 3),
-    (8, 18.3, 45.00, 45, 4),
-    (9, 9.2, 22.50, 27, 1),
-    (10, 14.7, 32.50, 38, 2),
-    (11, 6.0, 15.00, 22, 3);
+    (1, 369, 922.5, 300, 2),
+    (2, 369, 922.5, 305, 3),
+    (3, 369, 922.5, 300, 1),
+    (4, 359, 897.5, 300, 3),
+    (5, 29, 72.5, 27, 3),
+    (6, 377, 942.5, 310, 3),
+    (7, 358, 895, 296, 1),
+    (8, 358, 895, 296, 2),
+    (9, 378, 945, 309, 3),
+    (10, 6.3, 15.75, 10, 4),
+    (11, 4.5, 11.25, 7, 4);
     
 INSERT INTO Usa (FK_Passageiro_CPF, FK_Viagem_Id) VALUES
-	('40153021845', 1),
-    ('43870718692', 2),
-    ('98944351490', 3),
-    ('64812811058', 4),
-    ('38598277312', 5),
-    ('90897008200', 6),
-	('40153021845', 7),
-    ('43870718692', 8),
-    ('98944351490', 9),
-    ('64812811058', 10),
-    ('38598277312', 11);
+	('59362314029', 1),
+    ('10531043088', 2),
+    ('59362314029', 3),
+    ('10531043088', 4),
+    ('42738597009', 5),
+    ('98822414098', 6),
+	('29438733060', 7),
+    ('29438733060', 8),
+    ('78428732086', 9),
+    ('59362314029', 10),
+    ('42738597009', 11);
     
 INSERT INTO Urbana (Bairro_Partida, Bairro_Destino, FK_Viagem_Id) VALUES
-	('Floresta', 'Chacara das Pedras', 2),
-	('Petrópolis', 'Bela Vista', 4),
-	('Auxiliadora', 'Floresta', 6),
-	('Menino Deus', 'Moinhos de Ventos', 8),
-	('Floresta', 'Partenon', 10);
+    ('Partenon', 'Floresta', 9),
+	('Floresta', 'Passo da Areaia', 11);
     
 INSERT INTO Interurbana (Taxa_Adicional, Cidade_Partida, Cidade_Destino, FK_Viagem_Id) VALUES
-	(10, 'Ceilândia Norte (Ceilândia)', 'Jardim Santa Inêz', 1),
-	(10, 'Jardim Santa Inêz', 'Ceilândia Norte (Ceilândia)', 3),
-	(75, 'São Paulo', 'Rio de Janeiro', 5),
-	(980.20, 'Acre', 'Cidade Nova', 7),
-	(195.50, 'Presidente Kennedy', 'Ouro Preto', 9),
-	(35, 'Salvador', 'COHAB', 11);
+	(23.06, 'Porto Alegre', 'Erechim', 1),
+	(23.06, 'Erechim', 'Porto Alegre', 2),
+	(23.06, 'Porto Alegre', 'Erechim', 3),
+    (22.44, 'Erechim', 'Santa Maria', 4),
+	(23.06, 'Porto Alegre', 'Gravataí', 5),
+	(23.56, 'Gravataí', 'Erechim', 6),
+	(22.4, 'Santa Maria', 'Erechim', 7),
+	(22.4, 'Erechim', 'Gravataí', 8),
+	(23.06, 'Porto Alegre', 'Erechim', 10);
---
+++ 3_Semestre/Banco_de_Dados_II/Trabalho01/trips.js
@@ -0,0 +1,6 @@
+let passenger = Math.floor(Math.random() * 6) + 1
+console.log("Cidade partida: " + passenger)
+passenger = Math.floor(Math.random() * 6) + 1
+console.log("Cidade destino: " + passenger)
+let car = Math.floor(Math.random() * 4) + 1
+console.log("Carro: " + car)
