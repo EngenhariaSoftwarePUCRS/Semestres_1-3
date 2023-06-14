10191f330c337573b078447bb0b84272d42b55cc
Felipe Freitas Silva <eu.felipefreitassilva@gmail.com>
2023-06-14 04:08:18 -0300

    3S_ALESTII_T02
    exemplos

--- 3_Semestre/Algoritmos_e_Estruturas_de_Dados_II/src/Trabalho02/App.java
+++ 3_Semestre/Algoritmos_e_Estruturas_de_Dados_II/src/Trabalho02/App.java
@@ -3,7 +3,7 @@ package Trabalho02;
 public class App {
     public static void main(String[] args) {
         // Without extension (txt), should be located inside mapas/ folder
-        String fileName = args.length > 0 ? args[0] : "mapa4";
+        String fileName = args.length > 0 ? args[0] : "mapa_500_1000";
         int harbourAmount = 9;
 
         long startTime = System.nanoTime();
